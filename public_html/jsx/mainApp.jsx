ad71e75a6fdd6c4f52ca589575e789254524fa20
Kaj Risberg <kaj.risberg@helsinkinet.fi>
2020-05-27 13:07:56 +0300

    kuvaharjoituksia

--- public_html/jsx/mainApp.jsx
+++ public_html/jsx/mainApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {URL_BLOG} from './variables.jsx';
+import { URL_BLOG, URL_IMG } from './variables.jsx';
 
 export default class MainApp extends React.Component {
     constructor (props) {
@@ -9,8 +9,12 @@ export default class MainApp extends React.Component {
     render () {
         return (
             <div className="mainroot">
-				Hello React MainApp
+				<div>Hello React MainApp</div>
 				<div><a href={URL_BLOG} >Blogiin</a></div>
+				<div className="test_bgimage"></div>
+				<div className="test_layerimage">
+					<img className="test_layerimage__image" src={URL_IMG+"pelto.jpg"} alt="pelto"/>
+				</div>
             </div>
         );
     }
--- public_html/jsx/variables.jsx
+++ public_html/jsx/variables.jsx
@@ -1,2 +1,3 @@
 export const URL_BLOG = "file://C:/Users/risberg/Documents/opiskelu/projects/test1/public_html/blog.html";
 export const URL_INDEX = "file://C:/Users/risberg/Documents/opiskelu/projects/test1/public_html/index.html";
+export const URL_IMG = "file://C:/Users/risberg/Documents/opiskelu/projects/test1/public_html/img/";
