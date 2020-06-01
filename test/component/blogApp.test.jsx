9a02a4e911177c8e6426d74ad867daebe010f50a
Kaj Risberg <kaj.risberg@helsinkinet.fi>
2020-06-01 17:52:23 +0300

    sass tuotantoversion npm moduulit

--- test/component/blogApp.test.jsx
+++ test/component/blogApp.test.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+//import toJSON from 'enzyme-to-json';
 import BlogApp from '../../public_html/jsx/component/blogApp.jsx';
 
+
 test (
-	'should return html-code',
+	'should return html-code wit react test renderer',
 	function () {
 		const renderer = new ReactShallowRenderer();
 		renderer.render(<BlogApp />);
 		expect(renderer.getRenderOutput()).toMatchSnapshot();
 	}
+);
+
+
+test (
+	'pitäisi palauttaa html-koodin entsyymi testillä',
+	function () {
+		const wrapper = shallow(<BlogApp />);
+		//expect(toJSON(wrapper)).toMatchSnapshot();
+		expect(wrapper).toMatchSnapshot();
+	}
+
 );
