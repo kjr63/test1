f08df27f37b9eac05bfe5e8222198fb98e04300c
Kaj Risberg <kaj.risberg@helsinkinet.fi>
2020-06-02 11:54:20 +0300

    webpack tuotantokäännös

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,41 @@
 //console.log(__dirname);
 const path = require('path');
+
+//export funktio
+
+module.exports = function (env) {
+	const isProduction = ( env === 'production' );
+	return (
+		{
+			entry: {
+				app: "./public_html/jsx/component/app.jsx",
+				blog: "./public_html/jsx/component/blog.jsx"
+			},
+			mode: "development",
+			output: {
+				path: path.join(__dirname, 'public_html', 'js'),
+				filename: "[name].js"
+			},
+			module: {
+				rules: [{
+					loader: 'babel-loader',
+					test: /\.jsx$/,
+					exclude: /node_modules/
+				}//, {
+				 //   test: /\.scss$/,
+				 //   use: "sass-loader"
+				//}
+				]
+			},
+			devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+			devServer: {
+				contentBase: path.join(__dirname, 'public_html')
+			}
+		}
+	);
+}
+
+/*
 module.exports = {
     entry: {
 		app: "./public_html/jsx/component/app.jsx",
@@ -26,3 +62,4 @@ module.exports = {
 		contentBase: path.join(__dirname, 'public_html')
 	}
 }
+*/
